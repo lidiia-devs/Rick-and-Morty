9d3790561103bfa4c5ebbd789a583d3f65bfa82e
Lidiia Diachkovskaia <lyatlasova@gmail.com>
2025-03-20 21:04:48 -0400

    Initial Commit

---
+++ Rick and Morty/Rick_and_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_MortyApp.swift
+//  Rick and Morty
+//
+//  Created by Lidiia Diachkovskaia on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
